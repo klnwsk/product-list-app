93296fcee606f067cdd4d94eb8466c1b793f987a
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2018-02-11 19:24:46 +0100

    Update ProductList to display data fetched from server.

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { getProducts, setIndex } from "../state/productList"
 
 
 class ProductList extends React.Component {
 
 
+  handleClick = event => {
+    const target = event.currentTarget
+    const id = target.dataset.itemId
+
+    this.props.setIndex(id)
+  }
+
+  componentDidMount() {
+    this.props.getProducts()
+  }
 
 
   render() {
     return (
       <div>
-        <h2>Products list:</h2>
+        <h2>Products:</h2>
+
+        {
+          this.props.status.getting && <p>Getting products...</p>
+        }
+
+        {
+          this.props.status.error && <p>Woops! Something went wrong :(</p>
+        }
+
         <ul>
           {this.props.products.map(
-            item =>
+            (item, ind) =>
               <li
                 onClick={this.handleClick}
-                data-item-id={item.name}
+                data-item-id={ind}
+                key={ind}
               >
-                <Link to={`/details`}>
-                {item.name}
+                <Link to={`/details/${ind}`}>
+                  {item.number}
                 </Link>
               </li>
           )}
@@ -30,11 +51,17 @@ class ProductList extends React.Component {
   }
 }
 
-const mapStateToProps = state =>({
+const mapStateToProps = state => ({
   products: state.productList.data,
+  status: state.productList
 })
 
+const mapDispatchToProps = dispatch => ({
+  setIndex: (index) => dispatch(setIndex(index)),
+  getProducts: () => dispatch(getProducts())
+})
 
 export default connect(
   mapStateToProps,
+  mapDispatchToProps
 )(ProductList);
