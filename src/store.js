cd088930ce1e90367d2920dc8747eda01588331b
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2018-02-11 19:17:04 +0100

    Remove firebase as it's not needed anymore

--- src/store.js
+++ src/store.js
@@ -1,7 +1,6 @@
 import { compose, combineReducers, applyMiddleware, createStore } from 'redux'
 import thunk from 'redux-thunk'
-import persistState from 'redux-localstorage'
-import firebase from 'firebase'
+// import persistState from 'redux-localstorage'
 import productList from "./state/productList"
 
 
@@ -9,23 +8,13 @@ const reducer = combineReducers({
   productList
 })
 
-const config = {
-  apiKey: "AIzaSyBp2K_39Vb21w1odCNc147YNLbfhIdgUZ8",
-  authDomain: "product-list-45bca.firebaseapp.com",
-  databaseURL: "https://product-list-45bca.firebaseio.com",
-  projectId: "product-list-45bca",
-  storageBucket: "product-list-45bca.appspot.com",
-  messagingSenderId: "683787719844"
-}
-
-firebase.initializeApp(config)
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 
 const enhancer = composeEnhancers(
   applyMiddleware(thunk),
-    persistState([])
+    // persistState([])
 )
 
 const store = createStore(
