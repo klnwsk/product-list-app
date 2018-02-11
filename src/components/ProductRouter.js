e623fd146e6f87a71cad561bf50ad22729cf2228
Nikodem Kalinowski <nikodemkalinowski@o2.pl>
2018-02-11 19:17:32 +0100

    Rearrange Router for the app

---
+++ src/components/ProductRouter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import ProductList from './ProductList';
+import ProductDetails from "./ProductDetails";
+
+
+class ProductRouter extends React.Component {
+
+
+
+  render() {
+    return(
+      <Router>
+        <div>
+          <Route exact path="/" component={ProductList}/>
+          <Route path={`/details/:id`} component={ProductDetails}/>
+        </div>
+      </Router>
+    )
+  }
+}
+
+const mapStateToProps = state =>({
+  currentIndex: state.productList.index,
+})
+
+export default connect(
+  mapStateToProps
+)(ProductRouter)
--- src/index.js
+++ src/index.js
@@ -1,21 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ProductList from './components/ProductList';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'
 import store from './store'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import ProductDetails from "./components/ProductDetails";
+import ProductRouter from './components/ProductRouter'
+import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <div>
-        <Route exact path="/" component={ProductList}/>
-        <Route path="/details" component={ProductDetails}/>
-      </div>
-    </Router>
+    <ProductRouter/>
   </Provider>,
   document.getElementById('root')
 )
